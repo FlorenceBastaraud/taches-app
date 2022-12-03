fa6a9c7ab07d3e11c88e79f3fc019d357714402c
Florence Bastaraud <fbastaraud@yahoo.fr>
2022-12-03 21:24:44 +0100

    Updating old version of my todo app - 'tâches app'. New implementation. New UI. Working so far: Display of new todo and storing in window Local Storage.

--- assets/script.js
+++ assets/script.js
@@ -1,106 +1,116 @@
-let input = document.querySelector(".input");
-let ajout = document.querySelector(".ajout");
-let tachesDiv = document.querySelector(".taches");
+window.addEventListener('load', () => {
+  let todos = [];
 
-//Je stocke les taches dans ce tableau
-let tableauDeTaches = [];
-
-// Je vérifie s'il y a des taches dans le LocalStorage et si il y en a, je les récupère
-if (localStorage.getItem("taches")) {
-  tableauDeTaches = JSON.parse(localStorage.getItem("taches"));
-}
-
-// J'appelle la fonction d'obtention de données du stockage local
-getLocalStorageData();
-
-// Ici, j'ajoute une nouvelle tache quand le bouton d'ajout est actionné et que l'input n'est pas vide
-ajout.onclick = function () {
-  if (input.value !== "") {
-    ajoutTacheTableau(input.value); // J'ajoute la tâche au tableau des tâches
-    input.value = ""; // Je vide l'input
+  if(localStorage.getItem('todos')){
+    todos = JSON.parse(localStorage.getItem('todos'))
   }
-};
 
-// Evénements sur une tâche
-tachesDiv.addEventListener("click", (e) => {
-  // suppression de la tache en question
-  if (e.target.classList.contains("suPp")) {
-    // J'enlève la tache du Local Storage
-    suppTache(e.target.parentElement.getAttribute("data-id"));
-    // Je retire la tache de la page
-    e.target.parentElement.remove();
-  }
-  // L'état de la tache: "faite" ou "pas faite"
-  if (e.target.classList.contains("tache")) {
-    // Toggle "Fait" pour le tache
-    toggleEtatTache(e.target.getAttribute("data-id"));
-    // Toggle la classe "Fait" 
-    e.target.classList.toggle("fait");
-  }
-});
+  const formTodo = document.getElementById('new-todo-form');
+  const inputTodo = document.getElementById('new-todo-input');
+  const divTodos = document.getElementById('todos'); 
 
-function ajoutTacheTableau(tacheText) {
-  // Data de la tache
-  const tache = {
-    id: Date.now(),
-    title: tacheText,
-    completed: false,
-  };
-  // J'insère la tache à 'intérieur du tableau de tâches
-  tableauDeTaches.push(tache);
-  // J'ajoute toutes les tâches du tableau sur la page
-  ajouterTachesDuTableau(tableauDeTaches);
-  // J'ajoute les tâches au LocalStorage
-  ajouterTachesAuLocalStorage(tableauDeTaches);
-}
+  
+  displayTodos(todos);
 
-function ajouterTachesDuTableau(tableauDeTaches) {
-  // Div tache vide
-  tachesDiv.innerHTML = "";
-  // Je passe une boucle sur le tableau de tâches
-  tableauDeTaches.forEach((tache) => {
-    // Je crée une div et lui ajoute la classe "tache"
-    let div = document.createElement("div");
-    div.className = "tache";
-    // Je check si la tache est "faite" et si oui, je lui ajoute la class "fait"
-    if (tache.completed) {
-      div.className = "tache fait";
+
+  formTodo.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(!inputTodo.value){
+      alert('Renseignez une tâche à réaliser');
+      return;
     }
-    div.setAttribute("data-id", tache.id);
-    div.appendChild(document.createTextNode(tache.title));
-    // Je crée le bouton de suppression
-    let span = document.createElement("span");
-    span.className = "suPp";
-    span.appendChild(document.createTextNode("x"));
-    // J'ajoute le bouton dans la div
-    div.appendChild(span);
-    // Enfin, j'ajoute la div tache à la div contenair de l'ensemble des taches
-    tachesDiv.appendChild(div);
-  });
-}
 
-function ajouterTachesAuLocalStorage(tableauDeTaches) {
-  window.localStorage.setItem("taches", JSON.stringify(tableauDeTaches));
-}
-
-function getLocalStorageData() {
-  let data = window.localStorage.getItem("taches");
-  if (data) {
-    let taches = JSON.parse(data);
-    ajouterTachesDuTableau(taches);
-  }
-}
-
-function suppTache(tacheId) {
-  tableauDeTaches = tableauDeTaches.filter((tache) => tache.id != tacheId);
-  ajouterTachesAuLocalStorage(tableauDeTaches);
-}
-
-function toggleEtatTache(tacheId) {
-  for (let i = 0; i < tableauDeTaches.length; i++) {
-    if (tableauDeTaches[i].id == tacheId) {
-      tableauDeTaches[i].completed == false ? (tableauDeTaches[i].completed = true) : (tableauDeTaches[i].completed = false);
+    const newTodo = {
+      id: new Date(),
+      name: inputTodo.value.trim(),
+      state: false
     }
-  }
-  ajouterTachesAuLocalStorage(tableauDeTaches);
-}
+
+    todos.push(JSON.stringify(newTodo));
+  
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    
+    divTodos.innerHTML = "";
+    displayTodos(todos);
+
+    formTodo.reset();
+
+
+  })
+
+
+
+
+
+
+
+    function displayTodos(insertTodos){
+
+      insertTodos.map(todo => {
+
+          todo = JSON.parse(todo);
+          
+          const todoItem = document.createElement('div');
+          todoItem.classList.add('todo-item');
+      
+          const todoContent = document.createElement('div');
+          todoContent.classList.add('todo-content');
+      
+          const todoState = document.createElement('input');
+          todoState.setAttribute('type', 'checkbox');
+          todoState.classList.add('todo-state');
+      
+          const todoText = document.createElement('input');
+          todoText.setAttribute('type', 'text');
+          todoText.setAttribute('readonly', 'readonly');
+          todoText.classList.add('todo-text');
+          todoText.setAttribute('value', todo.name);
+      
+          const todoActions = document.createElement('div');
+          todoActions.classList.add('todo-actions');
+      
+          const todoEdit = document.createElement('span');
+          todoEdit.classList.add('todo-edit');
+      
+          const todoEditIcon = document.createElement('i');
+          todoEditIcon.classList.add('fa-solid');
+          todoEditIcon.classList.add('fa-pen-to-square');
+    
+          const todoDelete = document.createElement('span');
+          todoDelete.classList.add('todo-delete');
+      
+          const todoDeleteIcon = document.createElement('i');
+          todoDeleteIcon.classList.add('fa-solid');
+          todoDeleteIcon.classList.add('fa-trash');
+      
+      
+          todoItem.appendChild(todoContent);
+          todoItem.appendChild(todoActions);
+      
+          todoContent.appendChild(todoState);
+          todoContent.appendChild(todoText);
+      
+          todoActions.appendChild(todoEdit);
+          todoActions.appendChild(todoDelete);
+    
+          todoEdit.appendChild(todoEditIcon);
+    
+          todoDelete.appendChild(todoDeleteIcon);
+      
+          divTodos.appendChild(todoItem)
+      
+      
+      
+      
+        })
+     
+    }
+
+    
+    
+
+})
+
+
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Une application de listage de tâches réalisée avec Vanilla JS et LocalStorage | To Do application made with Vanilla JS and LocalStorage
 
-## By Florence B.
+#### By Florence B.
 
-### [Utilise l'application en cliquant sur ce lien](https://florencebastaraud.github.io/taches-app/)
+###### [Utilise l'application en cliquant sur ce lien](https://florencebastaraud.github.io/taches-app/)
